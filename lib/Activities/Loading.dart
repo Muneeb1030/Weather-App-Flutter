087f255db67b16e36f1806a71354a46155664a9c
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2023-07-13 16:28:25 +0500

    Internet Connectivity check and Toast Added

--- lib/Activities/Loading.dart
+++ lib/Activities/Loading.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:weatherapp/Model/Weather.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:connectivity/connectivity.dart';
 
 import './Home.dart';
 
@@ -18,13 +20,45 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    LoadData(City);
+    checkConnection().then((isConnected) {
+      if (isConnected) {
+        LoadData(City);
+      } else {}
+    });
+  }
+
+  Future<bool> checkConnection() async {
+    try {
+      var connectivityResult = await (Connectivity().checkConnectivity())
+          .timeout(Duration(seconds: 5));
+      if (connectivityResult == ConnectivityResult.none) {
+        Fluttertoast.showToast(
+            msg: "No Internet Connection",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+
+        return false;
+      }
+      return true;
+    } on TimeoutException catch (_) {
+      Fluttertoast.showToast(
+          msg: "Connection Timed Out",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+
+      return false;
+    }
   }
 
   void LoadData(String _city) async {
     WeatherInfo New = await WeatherInfo.empty().getInfo(_city);
-
-    await Future.delayed(Duration(seconds: 5));
+    await Future.delayed(Duration(seconds: 3));
 
     Navigator.pushReplacement(
       context,
@@ -37,12 +71,6 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(0),
-        child: AppBar(
-          backgroundColor: Colors.blue[300],
-        ),
-      ),
       body: Container(
         decoration: BoxDecoration(
           gradient: RadialGradient(
@@ -76,22 +104,6 @@ class _LoadingState extends State<Loading> {
                 color: Colors.white,
                 size: 80.0,
               ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text(
-                    "Author: Muneeb",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white70,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              )
             ],
           ),
         ),
